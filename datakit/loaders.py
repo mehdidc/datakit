ba2225c4431daa6c90b9a30d71db52f990639255
mehdi cherti <mehdi@cherti.name>
2017-01-13 10:52:27 +0100

    more documentation for load_numpy and load_hdf5

--- datakit/loaders.py
+++ datakit/loaders.py
@@ -9,7 +9,7 @@ _load_numpy_cache = {}
 def pipeline_load_numpy(iterator, filename,
                         cols=['X'],
                         start=0, nb=None, shuffle=False,
-                        chunk_size=128,
+                        chunk_size=None,
                         cache=True,
                         cache_dict=_load_numpy_cache,
                         random_state=None):
@@ -19,6 +19,12 @@ def pipeline_load_numpy(iterator, filename,
     Parameters
     ----------
 
+    iterator : starting iterator
+        not used, it is just to follow the API
+        of pipelines, which take an iterator and
+        return a transformed iterator. But load_numpy
+        should be the first step in the pipeline, so
+        at that point iterator=None.
     filename : str
         filename to load
     cols : list of str
@@ -31,35 +37,55 @@ def pipeline_load_numpy(iterator, filename,
         from start.
     shuffle : bool(default=False)
         whether to shuffle the data
+    chunk_size : int(default=None)
+        size of the chunk (nb of examples) to load into memory each time.
+        (it does not have to be necessarily the size of the mini-batch,
+         these two quantities are independent). if it is None, the size
+         of the chunk is the size of the data (load all the data into memory).
     cache : bool(default=True)
         whether to store loaded data in a cache for next calls
     cache_dict : dict
         where to store the cache
     random_state : int(default=None)
+        used to create RandomState object to shuffle 
+        the data with in case shuffle is True.
     """
     rng = np.random.RandomState(random_state)
-
+    # retrieve from cache if it exists
     if cache and filename in _load_numpy_cache:
         data = _load_numpy_cache[filename]
     else:
+        # normal behavior : when the object does not exist in the cache
         filename = format_with_env(filename)
         data = np.load(filename)
         if cache:
-            _load_numpy_cache[filename] = data
+            d = {}
+            for col in data.keys():
+                d[col] = data[col][:]
+            _load_numpy_cache[filename] = d
+    if nb is None:
+        nb = len(data[data.keys()[0]])
 
     if shuffle:
+        #WARNING
+        # when shuffled is True, we first shuffle the whole data.
+        # Then, a slice defined by (start, start+nb) is
+        # selected.
         indices = np.arange(len(data[cols[0]]))
         rng.shuffle(indices)
         data_shuffled = {}
         for c in cols:
-            data_shuffled[c] = data[c][indices]
+            data_shuffled[c] = data[c][indices][start:start + nb]
         data = data_shuffled
-
-    if nb is None:
-        nb = len(data[data.keys()[0]])
-
+    else:
+        d = {}
+        for c in cols:
+            d[c] = data[c][start:start+nb]
+        data = d
+    if chunk_size is None:
+        chunk_size = nb
     def iter_func():
-        for i in range(start, start + nb, chunk_size):
+        for i in range(0, nb, chunk_size):
             d = {}
             for c in cols:
                 d[c] = data[c][i:i + chunk_size]
@@ -75,7 +101,13 @@ def pipeline_load_hdf5(iterator, filename,
 
     Paramters
     ---------
-
+    
+    iterator : starting iterator
+        not used, it is just to follow the API
+        of pipelines, which take an iterator and
+        return a transformed iterator. But load_hdf5
+        should be the first step in the pipeline, so
+        at that point iterator=None.
     filename : str
         filename to load
     cols : list of str
@@ -86,10 +118,8 @@ def pipeline_load_hdf5(iterator, filename,
         the size of the data to read.
         if None, take everything starting
         from start.
-    buffer_size : int(default=128)
-        read buffer_size rows each time from the file
-    random_state : int(default=None)
-
+    chunk_size : int(default=128)
+        read chunk_size_size rows each time from the file
     """
 
     filename = format_with_env(filename)
