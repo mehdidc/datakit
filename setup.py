f5b3d6a672c16091a43e65d18a56456f1524749e
mehdi cherti <mehdi@cherti.name>
2017-08-11 10:51:39 +0200

    add requirements.txt and simplify setup

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
-from setuptools import setup
+from __future__ import print_function
+from setuptools import setup, find_packages
 
-setup(
-    name="Data kit",
-    version="0.1",
-    author="mehdi cherti",
-    author_email="mehdi@cherti.name",
-    description=(""),
-    license="BSD",
-    keywords="",
-    url="",
-    zip_safe=False,  # the package can run out of an .egg file
-    classifiers=['Intended Audience :: Science/Research',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved',
-                 'Programming Language :: Python',
-                 'Topic :: Software Development',
-                 'Topic :: Scientific/Engineering',
-                 'Operating System :: Microsoft :: Windows',
-                 'Operating System :: POSIX',
-                 'Operating System :: Unix',
-                 'Operating System :: MacOS'],
-    platforms='any',
-    packages=['datakit']
-)
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+setup(name='datakit',
+      version='0.0.1',
+      description='data loading/transform kit',
+      author='Mehdi Cherti',
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      author_email='mehdicherti@gmail.com',
+      )
